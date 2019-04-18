98da095700b0ceb74d2d48bc9380b62b35790922
Trish-V <35573600+Trish-V@users.noreply.github.com>
2019-04-19 00:18:17 +0530

    routing initilised (#3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { BannerCardComponent } from './components/banner-card/banner-card.component';
-import { NewsAreaComponent } from './components/news-area/news-area.component'; 
+import { FooterComponent } from './shared/footer/footer.component'; 
+import { BannerCardComponent } from './home/banner-card/banner-card.component';
+import { NewsAreaComponent } from './home/news-area/news-area.component';
 
 @NgModule({
   declarations: [
