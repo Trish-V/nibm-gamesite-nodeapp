718f50b3d6727266b887413914b85bec1033f9e8
Imesh <imeshpro90@outlook.com>
2019-05-07 19:23:02 +0530

    rendering engine changed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to StrikeEnginePlus!');
+    expect(page.getTitleText()).toEqual('Welcome to strikeengine!');
   });
 
   afterEach(async () => {
